1f06380de38398760c8ad3c0c59bdb5e615489f7
Hundreath <lucas_morin@icloud.com>
2025-05-23 22:24:18 +0200

    update

--- src/tab.rs
+++ src/tab.rs
@@ -1,15 +1,18 @@
-use std::io;
-
+use std::io::{self, Write}; 
 pub fn tab() {
     let mut to_do: Vec<String> = Vec::new();
 
     println!("Ajout dans la Todo");
     println!("Print 'exit' pour ne plus ajouter dans la ToDo");
 
+
     loop {
         let mut to_do_add = String::new();
 
-        // Permet de lire l'input de l'utilisateur
+        print!(" $ ");
+
+        io::stdout().flush().unwrap(); // <-- Ajout ici
+
         io::stdin()
             .read_line(&mut to_do_add)
             .expect("Failed to read ligne");
@@ -17,8 +20,10 @@ pub fn tab() {
         if to_do_add.trim() == "exit" {
             break;
         }
+
         to_do.push(to_do_add);
     }
+
 
     println!("Affichage de la Todo");
     for test in &to_do {
